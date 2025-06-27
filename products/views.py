60632c9f9f05c5f31b74295f722831054806617f
Ali Al_Ali <thek46802@gmail.com>
2025-06-27 16:23:36 +0300

    #7: changed the permission class

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from .models import Product
 from .serializers import ProductSerializer
 from .models import Purchase, Sale
@@ -10,7 +10,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # permission_classes = [IsAuthenticatedOrReadOnly]  # Optional: Add authentication later
-    permission_classes = [AllowAny]  # for now
+    permission_classes = [IsAuthenticated]  # for now
 
 class PurchaseViewSet(viewsets.ModelViewSet):
     queryset = Purchase.objects.all()
